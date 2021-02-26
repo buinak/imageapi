804811547a0304f6a30173da067308359a54c532
Mitchell Mullen <michalmullen@gmail.com>
2021-02-26 12:21:25 +0100

    feat: basic file upload

--- src/main/java/com/buinak/imageapi/controller/ImageController.java
+++ src/main/java/com/buinak/imageapi/controller/ImageController.java
@@ -4,11 +4,14 @@ import com.buinak.imageapi.entity.Image;
 import com.buinak.imageapi.entity.ImageData;
 import com.buinak.imageapi.repository.ImageDataRepository;
 import com.buinak.imageapi.repository.ImageRepository;
+import com.buinak.imageapi.service.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @Controller("/image")
@@ -16,17 +19,24 @@ public class ImageController {
 
     private final ImageRepository imageRepository;
     private final ImageDataRepository imageDataRepository;
+    private final StorageService storageService;
 
     @Autowired
-    public ImageController(ImageRepository imageRepository, ImageDataRepository imageDataRepository) {
+    public ImageController(ImageRepository imageRepository,
+                           ImageDataRepository imageDataRepository,
+                           StorageService storageService) {
         this.imageRepository = imageRepository;
         this.imageDataRepository = imageDataRepository;
+        this.storageService = storageService;
     }
 
     @PostMapping(path = "addImage")
     public ResponseEntity<Image> addImage(@RequestParam(defaultValue = "NAME") String name,
-                                          @RequestParam(defaultValue = "DESC") String description){
-        ImageData imageData = ImageData.builder().build();
+                                          @RequestParam(defaultValue = "DESC") String description,
+                                          @RequestParam() MultipartFile file) throws IOException {
+        ImageData imageData = ImageData.builder()
+                //.fullImage(file.getBytes())
+                .build();
         imageDataRepository.saveAndFlush(imageData);
 
         Image image = Image.builder()
@@ -34,6 +44,8 @@ public class ImageController {
                 .description(description)
                 .imageData(imageData)
                 .build();
+
+        storageService.uploadFile(file);
 
         return ResponseEntity.ok().body(imageRepository.saveAndFlush(image));
     }
---
+++ src/main/java/com/buinak/imageapi/service/StorageService.java
@@ -0,0 +1,31 @@
+package com.buinak.imageapi.service;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@Service
+public class StorageService {
+
+    @Value("${app.upload.dir:${user.home}}")
+    public String uploadDir;
+
+    public void uploadFile(MultipartFile file) {
+
+        try {
+            Path copyLocation = Paths
+                    .get(uploadDir + File.separator + StringUtils.cleanPath(file.getOriginalFilename()));
+            Files.copy(file.getInputStream(), copyLocation, StandardCopyOption.REPLACE_EXISTING);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
