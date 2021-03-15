93f50dcf2367997165127eb20ec36ccdc53103c1
Mitchell Mullen <michalmullen@gmail.com>
2021-03-15 10:24:35 +0100

    fix: test new save image

--- src/test/java/com/buinak/imageapi/controller/ImageControllerIT.java
+++ src/test/java/com/buinak/imageapi/controller/ImageControllerIT.java
@@ -2,7 +2,9 @@ package com.buinak.imageapi.controller;
 
 import com.buinak.imageapi.entity.Image;
 import com.buinak.imageapi.repository.ImageRepository;
+import com.buinak.imageapi.service.StorageService;
 import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +14,9 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -31,12 +31,15 @@ public class ImageControllerIT {
     @Autowired
     ImageRepository imageRepository;
 
+    @Autowired
+    StorageService storageService;
+
     final static String IMG_PATH = "src/test/java/com/buinak/imageapi/controller/testimg.jpeg";
 
 
     @Test
     public void addImage() throws IOException {
-        MultipartFile multipartFile = new MockMultipartFile("testimg.jpeg", new FileInputStream(new File(IMG_PATH)));
+        MultipartFile multipartFile = new MockMultipartFile("testimg.jpeg", new FileInputStream(IMG_PATH));
 
         imageController.addImage("NAME1", "DESC1", multipartFile);
 
@@ -47,20 +50,24 @@ public class ImageControllerIT {
 
     @Test
     public void listImages() throws IOException {
-        MultipartFile multipartFile = new MockMultipartFile("testimg.jpeg", new FileInputStream(new File(IMG_PATH)));
-        MultipartFile multipartFile2 = new MockMultipartFile("testimg2.jpeg", new FileInputStream(new File(IMG_PATH)));
-        imageController.addImage("NAME1", "DESC1", multipartFile);
-        imageController.addImage("NAME2", "DESC2", multipartFile2);
+        MockMultipartFile firstFile = new MockMultipartFile("data", "testimg.jpeg", "text/plain", "some xml".getBytes());
+        imageController.addImage("NAME1", "DESC1", firstFile);
+
+        MockMultipartFile secondFile = new MockMultipartFile("data", "testimg.jpeg", "text/plain", "some xml".getBytes());
+        imageController.addImage("NAME2", "DESC2", secondFile);
 
         List<String> images = imageController.getImageLinks().getBody();
+        System.out.println("-----------------------------------------------");
+        System.out.println(imageRepository.findByName("NAME1"));
+        System.out.println(images);
         assertThat(images.size()).isEqualTo(2);
-        assertThat(images.get(1)).isEqualTo("testimg.jpeg");
-        assertThat(images.get(2)).isEqualTo("testimg2.jpeg");
+        assertThat(images.get(0)).isEqualTo("testimg.jpeg");
+        assertThat(images.get(1)).isEqualTo("testimg2.jpeg");
     }
 
     @Test
     public void patchImage() throws IOException {
-        MultipartFile multipartFile = new MockMultipartFile("testimg.jpeg", new FileInputStream(new File(IMG_PATH)));
+        MockMultipartFile multipartFile = new MockMultipartFile("data", "testimg.jpeg", "text/plain", "some xml".getBytes());
         ResponseEntity<Image> responseImage = imageController.addImage("NAME2", "DESC2", multipartFile);
         Image image = responseImage.getBody();
         image.setDescription("string");
@@ -71,11 +78,13 @@ public class ImageControllerIT {
         Image image2 = imageController.findImageByName("string").getBody();
         assertThat(image2.getName()).isEqualTo("string");
         assertThat(image2.getDescription()).isEqualTo("string");
+
+
     }
 
     @Test
     public void deleteImage() throws IOException {
-        MultipartFile multipartFile = new MockMultipartFile("testimg.jpeg", new FileInputStream(new File(IMG_PATH)));
+        MockMultipartFile multipartFile = new MockMultipartFile("data", "testimg.jpeg", "text/plain", "some xml".getBytes());
         ResponseEntity<Image> responseImage = imageController.addImage("NAME3", "DESC3", multipartFile);
         Image image = responseImage.getBody();
         long id = image.getId();
